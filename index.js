91327447a1ce788e0f03d9705f775fb0c0093dc1
Devon Noel de Tilly <devon.noeldetilly@gmail.com>
2015-09-07 17:11:59 -0400

    404 and 500 error handling

--- index.js
+++ index.js
@@ -7,6 +7,14 @@ app.get('/', function(req, res){
   res.sendFile('index.html');
 });
 
+app.use(function(req, res){
+  res.sendFile(__dirname + '/public/404.html');
+});
+
+app.use(function(error, req, res, next) {
+  res.sendFile(__dirname + '/public/500.html');
+});
+
 var server = app.listen(5000, function(){
   var host = server.address().address;
   var port = server.address().port;
