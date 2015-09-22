c7a823af96f876a34416f3ebebf0e251b1b41b6e
Devon Noel de Tilly <devon.noeldetilly@gmail.com>
2015-09-22 11:30:56 -0400

    store emails in psql

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 var express     = require('express');
+var bodyParser  = require('body-parser');
 var favicon     = require('serve-favicon');
 var pg          = require('pg');
 var conString   = process.env.LANDING_DSN;
@@ -9,11 +10,15 @@ app.use(express.static('public'));
 
 app.use(favicon('public/images/favicon.ico'));
 
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
 app.get('/', function(req, res) {
   res.sendFile('index.html');
 });
 
 app.post('/submit', function(req, res) {
+  addEmail(req.body.email);
   res.redirect('/');
 });
 
@@ -31,13 +36,37 @@ var server = app.listen(5000, function() {
 
   console.log('Magic happens at http://%s:%s', host, port);
 
-  pgConnect();
+  createSignupsTable();
 });
 
-function pgConnect() {
+function createSignupsTable() {
   pg.connect(conString, function(err, client, done) {
     if(err) {
       return console.error('Error fetching client from pool', err);
     }
+
+    client.query('CREATE TABLE IF NOT EXISTS signups (id serial PRIMARY KEY NOT NULL, email text NOT NULL);', function(err, res) {
+      done();
+
+      if(err) {
+        return console.error('Error running query', err);
+      }
+    });
+  });
+}
+
+function addEmail(email) {
+  pg.connect(conString, function(err, client, done) {
+    if(err) {
+      return console.error('Error fetching client from pool', err);
+    }
+
+    client.query('INSERT INTO signups (email) SELECT $1 WHERE NOT EXISTS (SELECT id FROM signups WHERE email = $1);', [email], function(err, res) {
+      done();
+
+      if(err) {
+        return console.error('Error running query', err);
+      }
+    });
   });
 }
